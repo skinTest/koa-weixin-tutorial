9faeb34efcb95dc154bbdd2a8e05b33abb19805f
薛淞之 <287776261@qq.com>
2016-05-12 18:34:53 +0800

    微信公众号验证

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const Koa = require('koa')
 const sha1 = require('sha1')
 const config = {
-    pub_auth: {
+    weixin_auth: {
         appID: 'wx8abc2d9df3edb9dc',
         appSecret: 'f5fed50a6eb248d32e22b3ebe8a6ff31',
         token: 'ExilodasReallyAmazing'
@@ -14,6 +14,23 @@ const app = new Koa()
 
 app.use(function* (next) {
     console.log(this.query)
+    // get info for sha1 -> use es6 destructure assign to simplify this boilerplate
+    let token = config.weixin_auth.token
+    let signature = this.query.signature
+    let nonce = this.query.nonce
+    let timestamp = this.query.timestamp
+    let ecostr = this.query.ecostr
+
+    let str = [token, timestamp, nonce].sort().join('')
+    let sha = sha1(str)
+
+    if(sha === signature) {
+        this.body = ecostr + ''
+    } else {
+        console.log('some other requested')
+        console.dir(this.query)
+    }
+
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function () {
