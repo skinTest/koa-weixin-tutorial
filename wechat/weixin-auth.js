a83138f7fb7967579f0f4c9728752f9ff543514a
薛淞之 <287776261@qq.com>
2016-05-15 12:33:33 +0800

    show weixin post information

--- wechat/weixin-auth.js
+++ wechat/weixin-auth.js
@@ -5,6 +5,7 @@
  * the opts will include the wechat config
  */
 const sha1 = require('sha1')
+const getRawBofy = require('raw-body')
 const AccessToken = require('./access-token')
 
 module.exports = function(opts) {
@@ -21,13 +22,34 @@ module.exports = function(opts) {
         let str = [token, timestamp, nonce].sort().join('')
         let sha = sha1(str)
 
-        if(sha === signature) {
-            this.body = echostr + ''
-        } else {
-            this.body = 'it is not ok to hack me'
-            console.log('some other requested our server')
-            console.dir(this.query)
+/*!
+ * manage weixin server's request
+ * get: authorize with weixin
+ * post: event and so on
+ */
+        if (this.method === 'GET') {
+            if(sha === signature) {
+                this.body = echostr + ''
+            } else {
+                this.body = 'it is not ok to hack me'
+                console.log('some other requested our server')
+                console.dir(this.query)
+            }
         }
-        yield next
+        else if (this.method === 'POST') {
+            if(sha !== signature) {
+                this.body = 'it is not ok to hack me'
+                return false
+            }
+
+            // get data from request through raw-body
+            let data = yield getRawBofy(this.req, {
+                length: this.length,
+                limit: '1mb',
+                encoding: this.charset
+            })
+
+            console.log(data.toString())
+        }
     }
 }
