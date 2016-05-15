4f35c658b356b60bd4b9a678751b79465335eeaa
薛淞之 <287776261@qq.com>
2016-05-15 10:53:22 +0800

    access-token-module

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ const path = require('path')
 const fsp = require('./libs/fs-promisify')
 // const weixinAuth = require('./wechat/weixin-auth')
 const weixinAuth = require('./wechat/g')
-
+const config = require('./config')
 const wechat_file = path.join(__dirname, './config/wechat.txt')
-const config = {
-    weixinAuth: {
-        appID: 'wx8abc2d9df3edb9dc',
-        appSecret: 'f5fed50a6eb248d32e22b3ebe8a6ff31',
-        token: 'ExilodasReallyAmazing',
-        getAccessToken: function () {
-            return fsp.readFileAsync(wechat_file)
-        },
-        saveAccessToken: function (data) {
-            data = JSON.stringify(data)
-            console.log('data from save %s', data)
-            return fsp.writeFileAsync(wechat_file, data)
-        }
-    }
-}
+// const config = {
+//     weixinAuth: {
+//         appID: 'wx8abc2d9df3edb9dc',
+//         appSecret: 'f5fed50a6eb248d32e22b3ebe8a6ff31',
+//         token: 'ExilodasReallyAmazing',
+//         getAccessToken: function () {
+//             return fsp.readFileAsync(wechat_file)
+//         },
+//         saveAccessToken: function (data) {
+//             data = JSON.stringify(data)
+//             console.log('data from save %s', data)
+//             return fsp.writeFileAsync(wechat_file, data)
+//         }
+//     }
+// }
 
 
 const app = new Koa()
--- config.js
+++ config.js
@@ -1,15 +1,22 @@
-const fsp = require('./libs')
+'use strict'
+
+const fsp = require('./libs/fs-promisify')
 const path = require('path')
 
+const wechat_file = path.join(__dirname, './config/wechat.txt')
+
 module.exports = {
-    appID: 'wx8abc2d9df3edb9dc',
-    appSecret: 'f5fed50a6eb248d32e22b3ebe8a6ff31',
-    token: 'ExilodasReallyAmazing',
-    getAccessToken: function () {
-        return fsp.readFileAsync(wechat_file)
-    },
-    saveAccessToken: function (data) {
-        data = JSON.stringify()
-        return fsp.writeFileAsync(wechat_file)
+    weixinAuth: {
+        appID: 'wx8abc2d9df3edb9dc',
+        appSecret: 'f5fed50a6eb248d32e22b3ebe8a6ff31',
+        token: 'ExilodasReallyAmazing',
+        getAccessToken: function () {
+            return fsp.readFileAsync(wechat_file)
+        },
+        saveAccessToken: function (data) {
+            data = JSON.stringify(data)
+            console.log('data from save %s', data)
+            return fsp.writeFileAsync(wechat_file, data)
+        }
     }
 }
